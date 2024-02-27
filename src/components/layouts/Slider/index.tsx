769813ba07d941446a19c9cf80e7638f245f255c
MEERYEONG <dmb00@naver.com>
2024-02-27 17:40:39 +0900

    test commit

--- src/components/layouts/Slider/index.tsx
+++ src/components/layouts/Slider/index.tsx
@@ -3,33 +3,29 @@ import Link from 'next/link'
 
 const Slider = () => {
     return (
-        <div
-             style={{
-                 background:`#eaeaea`,
-             }}>
-            <Link href="http://thesen.moinus.kr/myAdmin/index.php" className={styles.headerLogo} style={{color:`#353c43`}}>
-                <span className={styles.headerLogoName} style={{color:`#353c43`}}>더센2</span>
-                <span className={styles.headerLogoEn} style={{color:`#353c43`}}>Admin</span>
-            </Link>
-            <div className={styles.headerToday}>
-                <span className={styles.headerTodayYear}>2024</span>
-                <span className={styles.headerTodayMonth}>02</span>
-                <span className={styles.headerTodayDay}>26</span>
-
-            </div>
+        <div></div>
+        // <div className="aside_second">
+        //         <span>고객문의 관리<span>〓</span></span><a href="http://thesen.moinus.kr/myAdmin/_shop_product_talk.list.php" className="menu" title="상품문의 관리" style="display:block">상품문의 관리</a><a href="http://thesen.moinus.kr/myAdmin/_shop_request.list.php" className="menu" title="고객문의 관리" style="display:block">고객문의 관리</a><a href="http://thesen.moinus.kr/myAdmin/_shop_request_partner.list.php" className="menu" title="제휴/광고문의 관리" style="display:block">제휴/광고문의 관리</a><a href="http://thesen.moinus.kr/myAdmin/_bbs.post_qna.list.php" className="menu" title="일대일문의 게시판" style="display:block">일대일문의 게시판</a><span>게시판 통합관리<span>〓</span></span><a href="http://thesen.moinus.kr/myAdmin/_bbs.post_notice.list.php" className="menu" title="공지사항 게시판" style="display:block">공지사항 게시판</a><a href="http://thesen.moinus.kr/myAdmin/_bbs.comment_mng.list.php" className="menu" title="댓글 통합관리" style="display:block">댓글 통합관리</a><span>기본관리<span>〓</span></span><a href="http://thesen.moinus.kr/myAdmin/_main_bonus_dashboard.php" className="menu_on" title="관리자 메인" style="display:block">관리자 메인</a><a href="http://thesen.moinus.kr/myAdmin/_entershop.entershop.form.php" className="menu" title="관리자 정보수정" style="display:block">관리자 정보수정</a><a href="http://thesen.moinus.kr/myAdmin/_entershop.admin.form.php" className="menu" title="서브관리자 정보수정" style="display:none">서브관리자 정보수정</a><a href="http://thesen.moinus.kr/myAdmin/_cardsys.sms.list.php" className="menu" title="문자충전관리" style="display:block">문자충전관리</a><a href="http://thesen.moinus.kr/myAdmin/_popup.list.php" className="menu" title="팝업관리" style="display:block">팝업관리</a><a href="http://thesen.moinus.kr/myAdmin/_config.agree.form.php" className="menu" title="약관 및 정책설정" style="display:block">약관 및 정책설정</a><a href="http://thesen.moinus.kr/myAdmin/_normalpage.list.php" className="menu" title="일반페이지 관리" style="display:block">일반페이지 관리</a><a href="http://thesen.moinus.kr/myAdmin/_admin.list.php" className="menu" title="서브관리자 관리" style="display:none">서브관리자 관리</a>
 
 
-            {/*<div style={{position:`absolute;margin-left:500px`, marginTop:`8px`}}>*/}
-            {/*    */}
 
-            {/*</div>*/}
+                
+        //         <div className="favorite_menu">
+        //           <a className="title" alt="즐겨찾는메뉴설정" title="즐겨찾는메뉴설정" href="http://thesen.moinus.kr/myAdmin/_favmenu.form.php"></a>
+        //           <div className="box">
+                    
 
 
-            <div className={styles.headerBtn}>
-                <Link href="http://thesen.moinus.kr/myAdmin/logout.php" className={styles.headerBtnLogout} title="로그아웃"></Link>
+						
 
-            </div>
-        </div>
+
+        //           </div>
+        //         </div>
+
+
+                
+
+        //       </div>
     )
 }
 
