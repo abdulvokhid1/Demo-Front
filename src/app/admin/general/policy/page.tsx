4f138fd1cf219234725d989aeedf2e4c53e1644f
MEERYEONG <dmb00@naver.com>
2024-03-21 15:24:16 +0900

    fix conflict

--- src/app/admin/general/policy/page.tsx
+++ src/app/admin/general/policy/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
 
-const View = dynamic(() => import('src/components/pages/general/Policy'), { ssr: false })
+const View = dynamic(() => import('@/components/pages/general/Policy'), { ssr: false })
 
 const Page = () => {
     return <View  />
--- src/app/admin/general/pop/page.tsx
+++ src/app/admin/general/pop/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
 
-const View = dynamic(() => import('src/components/pages/general/Pop'), { ssr: false })
+const View = dynamic(() => import('@/components/pages/general/Pop'), { ssr: false })
 
 const Page = () => {
     return <View  />
